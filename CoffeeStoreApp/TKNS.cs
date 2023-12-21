72f48401d5a7e404cc9004e6d88da26cb5c26e44
chalua <ivanhuynh45@gmail.com>
2023-12-21 13:32:46 +0700

    nha

---
+++ CoffeeStoreApp/TKNS.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCAFE
+{
+    public partial class TKNS : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=MSI\SQLEXPRESS;Initial Catalog=QL_CAFE1;Integrated Security=True");
+        public TKNS()
+        {
+            InitializeComponent();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+
+
+                cmd.CommandText = "select * from NHANVIEN";
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    string[] row = { dr["manv"].ToString(), dr["tennv"].ToString(), dr["chucvu"].ToString(), dr["phanquyen"].ToString(), dr["matkhau"].ToString() };
+
+
+                    if (dr["chucvu"].ToString() == "Nhân viên")
+                    {
+                        listView1.Items.Add(new ListViewItem(row));
+                    }
+                    else
+                    {
+                        listView2.Items.Add(new ListViewItem(row));
+                    }
+                }
+                dr.Close();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string ten = textBox2.Text;
+                string sdt = textBox3.Text;
+                string email = textBox4.Text;
+                string diaChi = textBox5.Text;
+                string chucVu = textBox6.Text;
+                string matKhau = textBox7.Text;
+                string ngayVaoLam = monthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd");
+                string phanQuyen = textBox8.Text;
+                string maQuanLy = textBox9.Text;
+
+
+                conn.Open();
+                string sqlInsert = "insert into NHANVIEN(tennv, sdt, email,diachi,chucvu, ngayvaolam, matkhau, maquanly, phanquyen) values(@ten, @sdt, @email, @diachi, @chucvu, @ngayvaolam, @matkhau, @maquanly, @phanquyen)";
+                SqlCommand cmd = new SqlCommand(sqlInsert, conn);
+                cmd.Parameters.AddWithValue("@ten", ten);
+                cmd.Parameters.AddWithValue("@sdt", sdt);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@diachi", diaChi);
+                cmd.Parameters.AddWithValue("@chucvu", chucVu);
+                cmd.Parameters.AddWithValue("@ngayvaolam", ngayVaoLam);
+                cmd.Parameters.AddWithValue("@matkhau", matKhau);
+                cmd.Parameters.AddWithValue("@phanquyen", phanQuyen);
+                cmd.Parameters.AddWithValue("@maquanly", maQuanLy);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    string[] row = { dr["manv"].ToString(), dr["tennv"].ToString(), dr["chucvu"].ToString(), dr["phanquyen"].ToString(), dr["matkhau"].ToString() };
+
+
+                    if (dr["chucvu"].ToString() == "Nhân viên")
+                    {
+                        listView1.Items.Add(new ListViewItem(row));
+                    }
+                    else
+                    {
+                        listView2.Items.Add(new ListViewItem(row));
+                    }
+
+                }
+                dr.Close();
+                listView1.Items.Clear();
+                listView2.Items.Clear();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+                textBox7.Text = "";
+                textBox8.Text = "";
+                textBox9.Text = "";
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi Thêm...");
+                listView1.Items.Clear();
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+        
+        
+        public void loadUser(string ma)
+        {
+           
+            conn.Open();
+            string sql = "select * from NHANVIEN where manv=@v1";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@v1", ma);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                textBox2.Text = dr["tennv"].ToString();
+                textBox3.Text = dr["sdt"].ToString();
+                textBox4.Text = dr["email"].ToString();
+                textBox5.Text = dr["diachi"].ToString();
+                textBox6.Text = dr["chucvu"].ToString();
+                textBox7.Text = dr["matkhau"].ToString();
+                textBox8.Text = dr["phanquyen"].ToString();
+                textBox9.Text = dr["maquanly"].ToString();
+                textBox10.Text = dr["manv"].ToString();
+                string dateString = dr["ngayvaolam"].ToString();
+
+               
+                DateTime date = DateTime.Parse(dateString);
+
+
+                monthCalendar1.SelectionStart = date;
+                monthCalendar1.SelectionEnd = date;
+                conn.Close();
+
+            }
+        }
+
+        private void listView2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            foreach (ListViewItem i in listView2.SelectedItems)
+            {
+                string ma = i.SubItems[0].Text;
+                loadUser(ma);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ListViewItem i in listView1.SelectedItems)
+            {
+                string ma = i.SubItems[0].Text;
+                loadUser(ma);
+            }
+        }
+        private void deleteUser(string id)
+        {
+           
+            conn.Open();
+            string sqlDelete = "delete from NHANVIEN where manv=@v1";
+            SqlCommand cmd = new SqlCommand(sqlDelete, conn);
+            cmd.Parameters.AddWithValue("@v1", textBox10.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            deleteUser(textBox10.Text);
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            LoadData();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+        }
+        public void updateNhanVien(string ma,string ten, string sdt, string email, string diaChi, string chucVu, string matKhau, string ngayVaoLam, string maQuanLy, string phanQuyen)
+        {
+            conn.Open();
+            string sqlUpdate = "update NHANVIEN set tennv = @v2, sdt= @v3, email = @v4, diachi=@v5, chucvu = @v6, ngayvaolam = @v7, matkhau = @v8, maquanly = @v9, phanquyen = @v10 where manv = @v1";
+            SqlCommand cmd = new SqlCommand(sqlUpdate, conn);
+            cmd.Parameters.AddWithValue("@v1", ma);
+            cmd.Parameters.AddWithValue("@v2", ten);
+            cmd.Parameters.AddWithValue("@v3", sdt);
+            cmd.Parameters.AddWithValue("@v4", email);
+            cmd.Parameters.AddWithValue("@v5", diaChi);
+            cmd.Parameters.AddWithValue("@v6", chucVu);
+            cmd.Parameters.AddWithValue("@v7", ngayVaoLam);
+            cmd.Parameters.AddWithValue("@v8", matKhau);
+            cmd.Parameters.AddWithValue("@v9", maQuanLy);
+            cmd.Parameters.AddWithValue("@v10", phanQuyen);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateNhanVien(textBox10.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text, textBox7.Text, monthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd"), textBox9.Text, textBox8.Text);
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            LoadData();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox1.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string ma = textBox10.Text;
+            conn.Open();
+            string sqlSearch = "select * from NhanVien where manv = @ma";
+            SqlCommand cmd = new SqlCommand(sqlSearch, conn);
+            cmd.Parameters.AddWithValue("@ma", ma);
+            SqlDataReader dr = cmd.ExecuteReader();
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            while (dr.Read())
+            {
+                string[] row = { dr["manv"].ToString(), dr["tennv"].ToString(), dr["chucvu"].ToString(), dr["phanquyen"].ToString(), dr["matkhau"].ToString() };
+
+
+                if (dr["chucvu"].ToString() == "Nhân viên")
+                {
+                    listView1.Items.Add(new ListViewItem(row));
+                }
+                else
+                {
+                    listView2.Items.Add(new ListViewItem(row));
+                }
+
+            }
+            textBox10.Clear();
+            conn.Close();
+        }
+
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
---
+++ CoffeeStoreApp/TKSP.designer.cs
@@ -0,0 +1,170 @@
+﻿namespace QLCAFE
+{
+    partial class TKSP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(25, 99);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(526, 322);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Enter += new System.EventHandler(this.chart1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Biểu đồ thể hiện số lượng khách hàng khi mua ở mỗi năm";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(588, 99);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(509, 322);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            this.chart2.Enter += new System.EventHandler(this.chart2_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(683, 435);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(297, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Biểu đồ thể hiện só lượng bán của từng sản phẩm";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkRed;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1137, 51);
+            this.textBox1.TabIndex = 97;
+            this.textBox1.Text = "Thống kê Sản Phẩm Và Khách Hàng";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1016, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 32);
+            this.button1.TabIndex = 98;
+            this.button1.Text = "Trả Về";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TKSP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "TKSP";
+            this.Text = "Thông Kê Sản Phẩm";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
